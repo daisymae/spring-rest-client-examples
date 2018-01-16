6073453f35330bf90fe9ad16e9578a4fa0e89a23
daisymae <daisymae@orcadiafarm.com>
2018-01-16 16:54:02 -0600

    Updated URL handling
    Modified the code so the URL is not hard-coded in the code, but instead
    uses a UriComponentsBuilder to build the URL from the property and any
    provided parameter values.

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,9 +1,11 @@
 package guru.springframework.springrestclientexamples.services;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
@@ -12,16 +14,21 @@ import guru.springframework.api.domain.UserData;
 public class ApiServiceImpl implements ApiService {
 
 	private RestTemplate restTemplate;
+	private final String api_url;
 
-	public ApiServiceImpl(RestTemplate restTemplate) {
+	public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
 		super();
 		this.restTemplate = restTemplate;
+		this.api_url = api_url;
 	}
 	
 	@Override
 	public List<User> getUsers(Integer limit) {
-		// url of where to retrieve the data, data class to bind to
-		UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder
+		    .fromUriString(api_url)
+				.queryParam("limit", limit);
+		// url updated to use a property
+		UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
 		return userData.getData();
 	}
 
